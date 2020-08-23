cc63f2949da66cac0de1763b8811fa3412ad7cd7
Kajal Singh <singh.kajal940@gmail.com>
2020-08-23 13:16:32 +0100

    Created income & Expense function constructors

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
- 
     
+    var Expense = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    
+    var data = {
+        allItem:{
+            exp: [],
+            inc: []
+        },
+        total:{
+            exp: 0,
+            inc: 0
+        }
+         
+    }
 })();
 
 

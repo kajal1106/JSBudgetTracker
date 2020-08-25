3f3a170f3d50c9819c934589255f989b6af1d5fa
Kajal Singh <singh.kajal940@gmail.com>
2020-08-25 22:19:11 +0100

    Deleting an Item from Budget controller

--- app.js
+++ app.js
@@ -67,6 +67,25 @@ var budgetController = (function () {
             // Return the new element
             return newItem;
         },
+
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            // id = 3
+            //data.allItems[type][id];
+            // ids = [1 2 4 6 8]
+            //index = 3
+
+            ids = data.allItems[type].map(function(current){
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
         
         calculateBudget: function() {
             
@@ -247,9 +266,10 @@ var UIController = (function(){
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1]; 
+            ID = parseInt(splitID[1]); 
 
             // 1. delete the item from the data structure
+                budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from UI
 
